6103967327e09963f365b4288d45094ab905e4d6
muzammilahmed10 <muzammilahmed100100@gmail.com>
2025-01-08 16:43:01 +0530

    Initial Commit

---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,3 @@
+#1. Python is Dynamically typed Programming language.
+#2. In python while Declaring variables we no need to mention datatypes.
+#3. DataTypes will be assigned by language based on values stored into the variables.
---
+++ VariablesDataTypes/Variables.py
@@ -0,0 +1,5 @@
+a=10
+b=20
+print("Addition of a and is:",a+b)
+b = "Muzammil"
+print(b)
