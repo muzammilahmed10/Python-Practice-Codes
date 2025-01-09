ec783b0d14a0f977b9ea9c295ca31140165d5012
muzammilahmed10 <muzammilahmed100100@gmail.com>
2025-01-09 16:39:48 +0530

    initial commit

--- Strings/Addition.py
+++ Strings/Addition.py
@@ -1,3 +1,7 @@
+#input() will always takes input as string
 num1 = int(input("Enter  num1\n"))
 num2 = int(input("Enter nums\n"))
 print(f"Addition of two numbers is:{num1+num2}")
+print(f"Subtraction of two numbers is:{num1-num2}")
+print(f"Multiplication of two nubes is:{num1*num2}")
+print(f"Division of two numbers is: {num1/num2}")
