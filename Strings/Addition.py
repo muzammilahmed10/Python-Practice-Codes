43b50841d2d04c4b384219e25bbbe2052661dd19
muzammilahmed10 <muzammilahmed100100@gmail.com>
2025-01-09 16:57:34 +0530

    intermedaiate commit

--- Strings/Addition.py
+++ Strings/Addition.py
@@ -1,4 +1,5 @@
 #input() will always takes input as string
+#Division result will always be of type float
 num1 = int(input("Enter  num1\n"))
 num2 = int(input("Enter nums\n"))
 print(f"Addition of two numbers is:{num1+num2}")
---
+++ TypeCasting.py
@@ -0,0 +1,13 @@
+# if String is holding integer it can be converted into int
+a = '30'
+print(a,type(a))
+b = int(a)
+print(b,type(b))
+#str to interger conversion is not allowed
+x = 'kod'
+print(x,type(x))
+# y = int(x)
+# print(y,type(y))
+
+p = float(input('Enter float type data')) #12.22
+print(p,type(p))
