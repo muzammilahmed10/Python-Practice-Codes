f2b1fc52791b175e9ee9062402cea857b60beb31
muzammilahmed10 <muzammilahmed100100@gmail.com>
2025-01-15 16:34:36 +0530

    'initialcommit'

--- controlConstructs/ForLoop.py
+++ controlConstructs/ForLoop.py
@@ -1,21 +1,26 @@
 '''
 for control_variable in iterable_object
 '''
-for i in 'Kodnest':
-    print(i)
-for j in [10,20,30]:
-    print(j+5)
+# for i in 'Kodnest':
+#     print(i)
+# for j in [10,20,30]:
+#     print(j+5)
 
-for num in range(1,11):
-    print(num)
+# for num in range(1,11):
+#     print(num)
 
-for num in range(11,21,2):
-    print(num,end=" ")
-print()
-for i in range(5):
-    print(i,end=" ")
-print()
-#Wap to print all even umbers from 1-10
-for num in range(1,10):
-    if(num%2==0):
-        print(num)
+# for num in range(11,21,2):
+#     print(num,end=" ")
+# print()
+# for i in range(5):
+#     print(i,end=" ")
+# print()
+# #Wap to print all even umbers from 1-10
+# for num in range(1,10):
+#     if(num%2==0):
+#         print(num)
+
+i=0
+while(i<10):
+    print(i+100)
+    i=i+1
---
+++ controlConstructs/jumping.py
@@ -0,0 +1,16 @@
+#WAp to print u from 1-10 but if number is 6 skip printing
+for i in range(1,11):
+    if(i==6):
+        continue
+    print(i)
+
+for i in range(1,11):
+    if(i==5):
+        break
+    print(i)
+
+def disp():
+    pass
+
+class Demo:
+    pass
